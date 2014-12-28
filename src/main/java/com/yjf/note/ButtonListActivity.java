6b518f4a705c500b18470a57b9182eb3b605af71
yinyuehuiok <1123766783@qq.com>
2014-12-28 18:12:31 +0800

    图片跑马灯实例主干代码

--- src/main/java/com/yjf/note/ButtonListActivity.java
+++ src/main/java/com/yjf/note/ButtonListActivity.java
@@ -3,15 +3,20 @@ package com.yjf.note;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 /**
  * Created by Administrator on 2014/12/26 0026.
  */
-public class ButtonListActivity extends Activity implements OnClickListener{
+public class ButtonListActivity extends Activity implements OnClickListener {
 
+    private Button btn_photo, btn_picture, btn_off;
     private Intent intent;
 
     @Override
@@ -19,13 +24,72 @@ public class ButtonListActivity extends Activity implements OnClickListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_button);
         intent = getIntent();
-       Button btn_photo = (Button)this.findViewById(R.id.photo_button);
-       Button btn_picture = (Button)this.findViewById(R.id.picture_button);
-       Button btn_off = (Button)this.findViewById(R.id.off_button);
+        btn_photo = (Button) this.findViewById(R.id.photo_button);
+        btn_picture = (Button) this.findViewById(R.id.picture_button);
+        btn_off = (Button) this.findViewById(R.id.off_button);
+
+        LinearLayout layout = (LinearLayout) findViewById(R.id.pop_layout);
+
+        layout.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "提示：点击窗口外部关闭窗口！", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_photo.setOnClickListener(this);
+        btn_picture.setOnClickListener(this);
+        btn_off.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.off_button:
+                finish();
+                break;
+            case R.id.photo_button:
+                try {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, 1);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+            case R.id.picture_button:
+                try {
+                    Intent intent = new Intent();
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(intent, 2);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+            default:
+                break;
+        }
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+       if(resultCode != RESULT_OK){
+           return;
+       }
+        if (data.getExtras() != null){
+               intent.putExtras(data.getExtras());
+        }
+        if(data.getData() != null){
+            intent.setData(data.getData());
+        }
+        setResult(1,intent);
+        finish();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        finish();
+        return super.onTouchEvent(event);
     }
 }
--- src/main/java/com/yjf/note/MainActivity.java
+++ src/main/java/com/yjf/note/MainActivity.java
@@ -2,28 +2,51 @@ package com.yjf.note;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private List<Uri> imageList;
+    private static int nowImage;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = (Button)findViewById(R.id.add_picture);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"button test",Toast.LENGTH_LONG).show();
-                startActivityForResult(new Intent(MainActivity.this,ButtonListActivity.class),1);
-            }
-        });
+        Button add_button = (Button)findViewById(R.id.add_picture);
+        Button before_button = (Button)findViewById(R.id.before_picture);
+        Button after_button = (Button)findViewById(R.id.after_picture);
+        add_button.setOnClickListener(this);
+        before_button.setOnClickListener(this);
+        after_button.setOnClickListener(this);
+        //初始化ImageView
+        initImageLayout();
+    }
+
+    //初始化ImageView
+    private void initImageLayout(){
+        RelativeLayout imageLayout = (RelativeLayout)findViewById(R.id.cycle);
+        TextView textView = new TextView(this);
+        textView.setText("还没有图片，点击上传按钮添加");
+        imageLayout.addView(textView);
+        //初始化图片数据
+        imageList = new ArrayList<Uri>();
+        //初始化图片索引值
+        nowImage = 0;
     }
 
     @Override
@@ -48,4 +71,46 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+       if(resultCode == 1){
+           if(data != null){
+               Uri imageUri = data.getData();
+               System.out.println(imageUri.getPath());
+               if(imageUri != null){
+               }
+           }
+       }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.add_picture:
+                startActivityForResult(new Intent(MainActivity.this,ButtonListActivity.class),1);
+                break;
+            case R.id.before_picture:getNextImage(0,nowImage);break;
+            case R.id.after_picture:getNextImage(1,nowImage);break;
+        }
+    }
+    //flag 1表示下一张，0表示上一张
+    private void getNextImage(int flag,int id){
+          if(flag == 1){
+              if(imageList.size() == id){
+                  Toast.makeText(this,"已经是最后一张了", Toast.LENGTH_SHORT).show();
+              }else{
+                  //返回下一张
+
+              }
+          }else{
+              if(id == 0){
+                  Toast.makeText(this.getApplicationContext(),"已经是第一张了", Toast.LENGTH_SHORT).show();
+              }else{
+                  //返回上一张
+
+              }
+          }
+    }
+
 }
